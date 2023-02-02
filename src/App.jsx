5e87af290b39a9d37ead964d5b95a8bee03a0b41
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-02 01:56:52 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState('red')
-
-
+  const [color, setColor] = useState(false)
   return (
     <div className="body">
-      <main style={{ backgroundColor: { color } }}>
-        <p onClick={() => setColor(red)}></p>
-        <p></p>
-        <p></p>
-        <p></p>
+      <main style={{ backgroundColor: color == 871111 ? '#871111' : color == '56628' ? '#5A6628' : color == '113287' ? '#113287' : color == '761187' ? '#761187' : '' }}>
+        <p onClick={() => setColor(871111)}></p>
+        <p onClick={() => setColor(56628)}></p>
+        <p onClick={() => setColor(113287)}></p>
+        <p onClick={() => setColor(761187)}></p>
       </main>
     </div >
     // #871111
