824b482e8a89360196fdd9d89f8a9e8dcc377fed
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-02 17:26:59 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   const [color, setColor] = useState('')
   return (
     <div className="body">
-      <main style={{ backgroundColor: color == 871111 ? '#871111' : color == '56628' ? '#5A6628' : color == '113287' ? '#113287' : color == '761187' ? '#761187' : '' }}>
-        <p onClick={() => setColor(871111)}></p>
-        <p onClick={() => setColor(56628)}></p>
-        <p onClick={() => setColor(113287)}></p>
-        <p onClick={() => setColor(761187)}></p>
+      <main style={{ backgroundColor: color }}>
+        <p onClick={() => setColor('#871111')}></p>
+        <p onClick={() => setColor('#5A6628')}></p>
+        <p onClick={() => setColor('#113287')}></p>
+        <p onClick={() => setColor('#761187')}></p>
       </main>
     </div >
   )
